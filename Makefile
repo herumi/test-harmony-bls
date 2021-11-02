9403d3dbb39a1ff81be83bc775beed15d716284c
MITSUNARI Shigeo <herumi@nifty.com>
2021-11-02 11:19:12 +0900

    add sign and verify test

--- Makefile
+++ Makefile
@@ -1,18 +1,20 @@
 OLD=old
 NEW=new
-all: harmony_test_old.exe harmony_test_new.exe
+TARGET=harmony_test_old.exe harmony_test_new.exe
 
+all: $(TARGET)
 
 SRC=harmony_test.cpp
 CFLAGS=-O2 -NDEBUG -Wall -Wextra
+LDFLAGS=-lgmp -lgmpxx -lcrypto -lpthread
 OLD_CFLAGS=-DBLS_SWAP_G -I $(OLD)/mcl/include -I $(OLD)/bls/include -DTEST_OLD
-OLD_LDFLAGS=-L $(OLD)/lib -L $(OLD)/mcl/lib
+OLD_LDFLAGS=-L $(OLD)/lib -L $(OLD)/mcl/lib $(LDFLAGS)
 
 NEW_CFLAGS=-DBLS_ETH -I $(NEW)/bls/include -I $(NEW)/bls/mcl/include -DTEST_NEW
-NEW_LDFLAGS=-L $(NEW)/lib -L $(NEW)/mcl/lib
+NEW_LDFLAGS=-L $(NEW)/lib -L $(NEW)/mcl/lib $(LDFLAGS)
 
-OLD_LIB=$(OLD)/bls/lib/libbls384_256.a
-NEW_LIB=$(NEW)/bls/lib/libbls384_256.a
+OLD_LIB=$(OLD)/bls/lib/libbls384_256.a $(OLD)/mcl/lib/libmcl.a
+NEW_LIB=$(NEW)/bls/lib/libbls384_256.a $(NEW)/bls/mcl/lib/libmcl.a
 
 $(OLD_LIB):
 	$(MAKE) -C $(OLD)/mcl
@@ -28,8 +30,15 @@ harmony_test_new.o: harmony_test.cpp
 	$(CXX) -c $< -o $@ -MMD -MP -MF $(@:.o=.d) $(NEW_CFLAGS)
 
 harmony_test_old.exe: harmony_test_old.o $(OLD_LIB)
-	$(CXX) $< -o $@ $(OLD_LDFLAGS) $(OLD_LIB)
+	$(CXX) $< -o $@ $(OLD_LIB) $(OLD_LDFLAGS)
 
 harmony_test_new.exe: harmony_test_new.o $(NEW_LIB)
-	$(CXX) $< -o $@ $(NEW_LDFLAGS) $(NEW_LIB)
+	$(CXX) $< -o $@ $(NEW_LIB) $(NEW_LDFLAGS)
 
+test: $(TARGET)
+	./harmony_test_old.exe > old.txt
+	./harmony_test_new.exe > new.txt
+	diff old.txt new.txt
+
+clean:
+	$(RM) -rf $(TARGET) *.o *.d
--- harmony_test.cpp
+++ harmony_test.cpp
@@ -1,12 +1,118 @@
 #include <stdio.h>
+#include <cybozu/xorshift.hpp>
+
+#ifdef TEST_OLD
+
+#ifndef BLS_SWAP_G
+	#error "define BLS_SWAP_G"
+#endif
+
+#define MCLBN_FP_UNIT_SIZE 6
+#define MCLBN_FR_UNIT_SIZE 4
+#include <bls/bls.h>
+
+#elif defined(TEST_NEW)
+
+#ifndef BLS_ETH
+	#error "define BLS_ETH"
+#endif
+#include <bls/bls384_256.h>
+
+#else
+	"error compile error"
+#endif
+
+#include <string>
+#include <vector>
+#include <cybozu/atoi.hpp>
+#include <cybozu/itoa.hpp>
+
+typedef std::vector<uint8_t> Uint8Vec;
+
+Uint8Vec fromHexStr(const std::string& s)
+{
+	Uint8Vec ret(s.size() / 2);
+	for (size_t i = 0; i < s.size(); i += 2) {
+		ret[i / 2] = cybozu::hextoi(&s[i], 2);
+	}
+	return ret;
+}
+
+std::string toHexStr(const uint8_t *buf, size_t n)
+{
+	std::string s;
+	for (size_t i = 0; i < n; i++) {
+		s += cybozu::itohex(buf[i], false);
+	}
+	return s;
+}
+
+void test_sign_verify(cybozu::XorShift& rg)
+{
+	uint8_t secBuf[32];
+	rg.read(secBuf, sizeof(secBuf));
+	blsSecretKey sec;
+	int ret;
+	ret = blsSecretKeySetLittleEndianMod(&sec, secBuf, sizeof(secBuf));
+	if (ret != 0) {
+		puts("err blsSecretKeySetLittleEndianMod");
+		return;
+	}
+	uint8_t buf[256];
+	int n = blsSecretKeySerialize(buf, sizeof(buf), &sec);
+	printf("sec=%s\n", toHexStr(buf, n).c_str());
+	blsPublicKey pub;
+	blsGetPublicKey(&pub, &sec);
+	n = blsPublicKeySerialize(buf, sizeof(buf), &pub);
+	printf("pub=%s\n", toHexStr(buf, n).c_str());
+	const size_t msgSize = 32;
+	uint8_t msg[msgSize];
+	rg.read(msg, msgSize);
+	printf("msg=%s\n", toHexStr(msg, msgSize).c_str());
+	blsSignature sig;
+	blsSign(&sig, &sec, msg, msgSize);
+	n = blsSignatureSerialize(buf, sizeof(buf), &sig);
+	printf("sig=%s\n", toHexStr(buf, n).c_str());
+	ret = blsVerify(&sig, &pub, msg, msgSize);
+	if (ret != 1) {
+		puts("err blsVerify");
+		return;
+	}
+}
 
 int main()
 {
 #ifdef TEST_OLD
-	puts("old test");
-#elif defined(TEST_NEW)
-	puts("new test");
+	fprintf(stderr, "old test\n");
 #else
-	"error compile error"
+	fprintf(stderr, "new test\n");
 #endif
+	int ret = blsInit(MCL_BLS12_381, MCLBN_COMPILED_TIME_VAR);
+	if (ret != 0) {
+		puts("err blsInit");
+		return 1;
+	}
+#ifdef TEST_NEW
+	blsSetETHserialization(0);
+	blsSetMapToMode(0);
+	{
+		const char *genStr = "1 4f58f3d9ee829f9a853f80b0e32c2981be883a537f0c21ad4af17be22e6e9959915ec21b7f9d8cc4c7315f31f3600e5 1212110eb10dbc575bccc44dcd77400f38282c4728b5efac69c0b4c9011bd27b8ed608acd81f027039216a291ac636a8";
+		blsPublicKey gen;
+		ret = blsPublicKeySetHexStr(&gen, genStr, strlen(genStr));
+		if (ret != 0) {
+			puts("err blsPublicKeySetHexStr");
+			return 1;
+		}
+		ret = blsSetGeneratorOfPublicKey(&gen);
+		if (ret != 0) {
+			puts("err blsSetGeneratorOfPublicKey");
+			return 1;
+		}
+	}
+#endif
+	cybozu::XorShift rg;
+	for (int i = 0; i < 3; i++) {
+		test_sign_verify(rg);
+	}
 }
+
